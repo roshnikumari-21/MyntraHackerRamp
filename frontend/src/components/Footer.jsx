ec6c2d84b7ea98d5a2a768247e1e9b4cc4ee7495
Roshni Kumari <roshnikumari212004@gmail.com>
2025-09-14 01:16:51 +0530

    changed footer style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-8 mt-10">
+    <footer className="bg-red-100 text-black py-8 mt-10">
       <div className="max-w-7xl mx-auto px-6 text-center">
         <p className="text-sm">
           Made with love by ButterFlies ❤️
