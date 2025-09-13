54e3a09161bf3e99b4e50711011b1ca2b128a294
aprajita <aprajitakumarithakur5555@gmail.com>
2025-09-13 23:38:49 +0530

    Added padding to cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -34,7 +34,7 @@ const Cart = () => {
   return (
     <>
     <Navbar/>
-    <div className='border-t pt-14'>
+    <div className='border-t px-[20px] pt-14'>
 
       <div className=' text-2xl mb-3'>
         <Title text1={'YOUR'} text2={'CART'} />
